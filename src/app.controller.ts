ce8f71998aabe1eabcd61f1818d35cad035fb917
Алексей Казантаев <a.kazantaev@gmail.com>
2020-08-04 18:04:02 +0300

    log widget rest added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,22 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getlog();
+  constructor(private readonly appService: AppService) {
+  }
+  @Get('loglistener')
+  getListen(): string {
+    return this.appService.getLogListener();
+  }
+  @Get('change')
+  getChange(): boolean {
+    return this.appService.getLogChange();
+  }
+  @Get('data')
+  getLog(): string {
+    return this.appService.getLogData();
+  }
+  @Get('delete')
+  delLog(): boolean {
+    return this.appService.getLogDelete();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,63 @@
 import { Injectable } from '@nestjs/common';
-import {readFileSync} from 'fs';
+import { watch } from 'fs';
+import { execSync } from 'child_process';
 
 @Injectable()
 export class AppService {
-  getlog() : string {
-    const data = readFileSync('.env').toString();
-    const re = /\n/g;
-    const newstr = data.replace(re, "<br />");
-    return newstr;
+  first_watch = true;
+  change = 0;
+  listeners = 0;
+  watcher;
+
+  getDataHelp(): string{
+    function run(cwd, command) {
+      return execSync(command, { cwd, encoding: "utf8" });
+    }
+    function getData(cwd) {
+      return run(cwd, 'tail -n 10 /Users/air/backend-v0.1/textLogFile');
+    }
+    let data = getData('src');
+    //console.log(data);
+    return data;
+  }
+
+  getLogData() : string {
+
+    // console.log(this.change + '--')
+    this.change--;
+    let data = this.getDataHelp();
+    return data;
+  }
+
+  getLogListener() : string {
+    this.listeners++;
+    this.change++;
+    // console.log('listeners++ = ' + this.listeners)
+    return this.getDataHelp();
+  }
+
+  getLogChange() : boolean {
+    if (this.first_watch === true) {
+      this.watcher = watch('textLogFile', (eventType, filename) => {
+        if (filename) {
+          // console.log(`${filename} file Changed`);
+          // console.log(this.listeners);
+          // console.log('this.change += this.listeners');
+          this.change += this.listeners;
+          // console.log('this.change = ' + this.change);
+        }
+      });
+      // console.log('this.first_watch = false')
+      this.first_watch = false;
+    }
+      // console.log('getLogChange ' + (this.change > 0))
+      return (this.change > 0);
+  }
+
+  getLogDelete() : boolean {
+    this.listeners--;
+    // console.log(this.first_watch);
+    this.first_watch = true;
+    return false;
   }
 }
